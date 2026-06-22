332ed4eed11b4f2bf655e71669eb916355a61295
Kimanxo <kimanxo@yandex.ru>
2026-06-22 14:49:58 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://kimanxo.github.io",
-  base: "/shellbees",
+  base: "/shellbees/",
   vite: {
     plugins: [tailwindcss()],
   },
